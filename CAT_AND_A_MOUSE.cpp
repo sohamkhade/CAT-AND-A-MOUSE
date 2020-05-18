e2f5223ac4857dafed030c79980f6e010d6d5f13
sohamkhade2000 <55399257+sohamkhade2000@users.noreply.github.com>
2020-05-18 21:53:54 +0530

    Create CAT_AND_A_MOUSE.cpp

---
+++ CAT_AND_A_MOUSE.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,q;
+    cin>>q;int x=0,y=0;
+    for(int i=0;i<q;i++)
+    {
+        cin>>a>>b>>c;
+        x=c-a;
+        y=c-b;
+        if(y<0)
+            y=y*(-1);
+        if(x<0)
+            x=x*(-1);
+        if(x>y)
+            cout<<"Cat B"<<endl;
+        if(y>x)
+            cout<<"Cat A"<<endl;
+        if(x==y)
+            cout<<"Mouse C"<<endl;
+    }
+    return 0;
+}
